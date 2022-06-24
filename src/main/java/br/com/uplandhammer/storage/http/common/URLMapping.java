1e639e99cdb4c02f93158969b8c5f5895219c88b
UpLandHammer <brunorodrigues.tec.info@gmail.com>
2022-06-24 15:25:49 -0300

    :fix: Adicionando versão correta springfox; Ajustando api's.

--- src/main/java/br/com/uplandhammer/storage/http/common/URLMapping.java
+++ src/main/java/br/com/uplandhammer/storage/http/common/URLMapping.java
@@ -6,13 +6,13 @@ import lombok.experimental.UtilityClass;
 public class URLMapping {
     public static final String API = "api/storage";
     //BUCKET
-    public static final String BUCKET_CREATE = "/bucket/create";
-    public static final String BUCKET_LIST_ALL = "/bucket/all";
-    public static final String BUCKET_REMOVE = "/bucket/remove/{bucketName}";
+    public static final String BUCKET_CREATE = "/bucket";
+    public static final String BUCKET_LIST_ALL = "/buckets";
+    public static final String BUCKET_REMOVE = "/bucket/{bucketName}";
     //FILE
     public static final String FILE_UPLOAD = "/file/upload/{bucketName}";
     public static final String FILE_GET_INFO = "/file/info/{bucketName}";
-    public static final String FILE_GET_BY_BUCKET = "/file/{bucketName}/{fileName}";
-    public static final String FILE_REMOVE_FILE_BY_BUCKET = "/file/remove/{bucketName}/{fileName}";
+    public static final String FILE_GET_BY_BUCKET = "/file/{fileName}/bucket/{bucketName}";
+    public static final String FILE_REMOVE_FILE_BY_BUCKET = "/file/{fileName}/bucket/{bucketName}";
 
 }
--- src/main/java/br/com/uplandhammer/storage/usecase/ListAllBuckets.java
+++ src/main/java/br/com/uplandhammer/storage/usecase/ListAllBuckets.java
@@ -3,10 +3,12 @@ package br.com.uplandhammer.storage.usecase;
 import br.com.uplandhammer.storage.http.domain.response.CreateBucketResponse;
 import br.com.uplandhammer.storage.service.StorageContentFileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ListAllBuckets {
@@ -14,6 +16,8 @@ public class ListAllBuckets {
     private final StorageContentFileService storageContentFileService;
 
     public List<CreateBucketResponse> execute() {
+
+        log.info("Listando todo conteúdo.");
         return storageContentFileService.listAllBuckets();
     }
 }
